4aec25da7f1b40a0a2ea3a68b577c7bb32e36fb1
Imam Hidayat <imhidayat100602@gmail.com>
2023-05-19 18:48:59 +0700

    Install Bootstrap and Sass

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
+//PAGES
+import Home from "./pages/home";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <main />,
+    element: <Home />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider route={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+import "../sass/home.scss";
+
+export default function Home() {
+  return (
+    <div className="d-grid container">
+      <h1 className="text-danger">Welcome Home!</h1>
+    </div>
+  );
+}
