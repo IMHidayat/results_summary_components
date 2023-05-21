c394448290e5bdade1d0ec5f20f9a948d95a5b72
Imam Hidayat <imhidayat100602@gmail.com>
2023-05-21 19:33:05 +0700

    Fix : Adding corner border resultbar

--- src/components/resultsBar.jsx
+++ src/components/resultsBar.jsx
@@ -1,6 +1,14 @@
 export default function ResultBar(p) {
   return (
     <div className={`result-bar flex p-3 md:p-2 mb-5 md:mb-3 ${p.c} ${p.c3} `}>
+      {/* <span className="borderz">
+        <span className="ss"></span>
+        <span className="se"></span>
+        <span className="es"></span>
+        <span className="ee"></span>
+      </span> */}
+      <span className="border-top"></span>
+      <span className="border-bottom"></span>
       <div className="flex">
         <img className="me-3 md:mx-2" src={p.img} alt="Icon" />
         <span className={`text-xl md:text-base ${p.c2}`}>{p.name}</span>
